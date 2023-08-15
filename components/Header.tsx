c40720ecdf742393592772c9b282f294c1184717
Munazar Ali <munazar.ali.001@gmail.com>
2023-08-16 00:59:05 +0200

    build out the header and navbar components

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,12 @@
+import { AcademicCapIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import React from "react";
 
 function Header() {
   return (
-    <header className="w-full bg-black h-[15vh]">
-      <section className="py-2 flex items-center gap-14 max-w-7xl">
-        <Image
-          className="relative invert ml-2"
-          src="/graduation-cap.jpg"
-          alt="App Logo"
-          width={90}
-          height={18}
-          priority
-        />
+    <header className="w-full  bg-[rgb(35,42,45)] h-[14%]">
+      <section className="py-2 flex items-center gap-10 max-w-7xl ml-6 h-full">
+        <AcademicCapIcon className='ml-8 h-[7vh] fill-white'/>
         <h1 className="text-white text-3xl">Academia Hub</h1>
       </section>
     </header>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,19 +1,28 @@
 import React from 'react'
-import { HomeIcon } from '@heroicons/react/24/solid'
-
+import { HomeIcon, UserIcon } from '@heroicons/react/24/solid'
+import { ListBulletIcon } from '@heroicons/react/20/solid'
+import Link from 'next/link'
 
 function Navbar() {
   return (
-    <nav className="border border-black w-1/5  h-[85vh] flex flex-col  gap-8">
-        <section className='text-center border border-b-black'>
-        <h1 className='font-extrabold text-xl w-full'>Navigation</h1>
-        </section>
-        <section className='h-full'>
-            <article className='flex gap-4'>
-                <HomeIcon className='h-[4vh]'/>
-                <h4 className='font-semibold place-self-end'>Home</h4>
-            </article>
-        </section>
+    <nav className="w-1/5 relative h-[86%] flex flex-col gap-16 bg-gray-100">
+      <section className='text-center border border-b-black p-5'>
+        <h1 className='font-extrabold text-xl w-full text-left'>Navigation</h1>
+      </section>
+      <section className='h-full flex flex-col gap-12'>
+        <Link href={'/hello'} className='flex gap-4 ml-11'>
+          <HomeIcon className='h-[4vh] fill-[rgb(35,42,45)]'  />
+          <h4 className='place-self-end '>Home</h4>
+        </Link>
+        <Link href={'/hello'} className='flex gap-4 ml-11'>
+          <ListBulletIcon className='h-[4vh] fill-[rgb(35,42,45)]' />
+          <h4 className='place-self-end'>Students List</h4>
+        </Link>
+        <Link href={'/hello'} className='flex gap-4 ml-11'>
+          <UserIcon className='h-[4vh] fill-[rgb(35,42,45)]' />
+          <h4 className='place-self-end'>Manage Students</h4>
+        </Link>
+      </section>
     </nav>
   )
 }
