e322522e1b91518cf4b60279b01f77ffa6679370
Munazar Ali <munazar.ali.001@gmail.com>
2023-08-19 21:18:28 +0200

    build out the Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ function Header() {
     <header className="w-full  bg-[rgb(35,42,45)] h-[14%]">
       <section className="py-2 flex items-center gap-10 max-w-7xl ml-6 h-full">
         <AcademicCapIcon className='ml-8 h-[7vh] fill-white'/>
-        <h1 className="text-white text-3xl">Academia Hub</h1>
+        <h1 className="text-white text-3xl">AcademiaHub</h1>
       </section>
     </header>
   );
