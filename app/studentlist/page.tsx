a84d6ff894abccb881846518b6e3825abcdf5c27
Munazar Ali <munazar.ali.001@gmail.com>
2023-08-19 21:18:02 +0200

    build out the studentlist  page

---
+++ app/studentlist/page.tsx
@@ -0,0 +1,13 @@
+import DataTable from "@/components/studentList/DataTable";
+
+const StudentList: React.FC = () => {
+    return (
+        <main className='w-4/5 h-[87%] bg-gray-100'>
+            <section className='h-full  w-full pt-3 px-3'>
+              <DataTable/>
+            </section>
+        </main>
+    )
+};
+
+export default StudentList;
