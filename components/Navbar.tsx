071030e00ed7b82a577eb33e125c59c37202e673
Munazar Ali <munazar.ali.001@gmail.com>
2023-08-19 21:19:00 +0200

    configure the links in the navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,20 +5,20 @@ import Link from 'next/link'
 
 function Navbar() {
   return (
-    <nav className="w-1/5 relative h-[86%] flex flex-col gap-16 bg-gray-100">
-      <section className='text-center border border-b-black p-5'>
+    <nav className="w-1/5 relative h-[87%] flex flex-col gap-16 bg-gray-100">
+      <section className='text-center border border-b-gray-400 p-5'>
         <h1 className='font-extrabold text-xl w-full text-left'>Navigation</h1>
       </section>
       <section className='h-full flex flex-col gap-12'>
-        <Link href={'/hello'} className='flex gap-4 ml-11'>
+        <Link href={'/'} className='flex gap-4 ml-11'>
           <HomeIcon className='h-[4vh] fill-[rgb(35,42,45)]'  />
           <h4 className='place-self-end '>Home</h4>
         </Link>
-        <Link href={'/hello'} className='flex gap-4 ml-11'>
+        <Link href={'/studentlist'} className='flex gap-4 ml-11'>
           <ListBulletIcon className='h-[4vh] fill-[rgb(35,42,45)]' />
           <h4 className='place-self-end'>Students List</h4>
         </Link>
-        <Link href={'/hello'} className='flex gap-4 ml-11'>
+        <Link href={'/managestudents'} className='flex gap-4 ml-11'>
           <UserIcon className='h-[4vh] fill-[rgb(35,42,45)]' />
           <h4 className='place-self-end'>Manage Students</h4>
         </Link>
